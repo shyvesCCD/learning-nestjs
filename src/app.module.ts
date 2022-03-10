55ecefec161070047869c7dda8120b3c532559c6
shyvesCCD <pedroochavess97@gmail.com>
2022-03-10 00:22:25 -0300

    feat: enable pipe decorators globaly

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { MealsModule } from './meals/meals.module';
+import { MealsCategoriesModule } from './meals-categories/meals-categories.module';
+import { SituationsModule } from './situations/situations.module';
+import { OrdersModule } from './orders/orders.module';
+import { OrderHasMealsModule } from './order-has-meals/order-has-meals.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [AuthModule, UsersModule, PrismaModule, MealsModule, MealsCategoriesModule, SituationsModule, OrdersModule, OrderHasMealsModule],
 })
 export class AppModule {}
